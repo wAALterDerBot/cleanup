085b063cd53336063b016066619cc03ed2c54162
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 01:43:16 +0200

    Update shoko.py

--- anime_cleanup/shoko.py
+++ anime_cleanup/shoko.py
@@ -1,1 +1,20 @@
+from __future__ import annotations
 
+from anime_cleanup.config import ShokoConfig
+
+from anime_cleanup.api.client import ApiClient
+from anime_cleanup.api.auth import Authentication
+from anime_cleanup.api.series import SeriesApi
+from anime_cleanup.api.files import FilesApi
+
+
+class ShokoClient:
+
+    def __init__(self, config: ShokoConfig):
+
+        self.client = ApiClient(config)
+
+        Authentication(self.client).login()
+
+        self.series = SeriesApi(self.client)
+        self.files = FilesApi(self.client)
