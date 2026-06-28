4c1b5751fbf3ad026f299b43a616f214b80fed81
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 02:19:32 +0200

    Update auth.py

--- shokotoolkit/api/auth.py
+++ shokotoolkit/api/auth.py
@@ -1,1 +1,33 @@
+from __future__ import annotations
 
+from anime_cleanup.api.client import ApiClient
+
+
+class Authentication:
+
+    def __init__(self, client: ApiClient):
+
+        self.client = client
+
+    def login(self):
+
+        payload = {
+            "user": self.client.config.username,
+            "pass": self.client.config.password,
+            "device": "shoko-toolkit",
+        }
+
+        result = self.client.post(
+            "/auth",
+            payload,
+        )
+
+        token = result["token"]
+
+        self.client.session.headers.update(
+            {
+                "Authorization": f"Bearer {token}"
+            }
+        )
+
+        return token
