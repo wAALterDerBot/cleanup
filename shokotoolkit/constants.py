670057e5a533abcf5c33f71b9ab41a61f6ce3f2b
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 11:10:38 +0200

    Update constants.py

--- shokotoolkit/constants.py
+++ shokotoolkit/constants.py
@@ -1,1 +1,15 @@
+"""
+Global constants used by the toolkit.
+"""
 
+from pathlib import Path
+
+APP_NAME = "Shoko Toolkit"
+
+APP_VERSION = "0.1.0"
+
+LOG_DIR = Path("logs")
+
+LOG_FILE = LOG_DIR / "shokotoolkit.log"
+
+DEFAULT_TIMEOUT = 30
