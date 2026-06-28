47a171822a74c0aada7c935e9eb7183d37dc0811
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 11:10:53 +0200

    Update exceptions.py

--- shokotoolkit/exceptions.py
+++ shokotoolkit/exceptions.py
@@ -1,1 +1,27 @@
+"""
+Custom exceptions.
+"""
 
+
+class ShokoToolkitError(Exception):
+    """Base exception."""
+
+
+class ConfigurationError(ShokoToolkitError):
+    """Configuration is invalid."""
+
+
+class AuthenticationError(ShokoToolkitError):
+    """Authentication failed."""
+
+
+class ApiError(ShokoToolkitError):
+    """API request failed."""
+
+
+class ScannerError(ShokoToolkitError):
+    """Library scanner failed."""
+
+
+class CleanupError(ShokoToolkitError):
+    """Cleanup failed."""
