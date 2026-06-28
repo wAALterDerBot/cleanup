042aea5a89b35ac6dd030adf6b4d2b0901b889bf
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 02:20:14 +0200

    Update series.py

--- shokotoolkit/api/series.py
+++ shokotoolkit/api/series.py
@@ -1,1 +1,20 @@
+from __future__ import annotations
 
+from anime_cleanup.api.client import ApiClient
+
+
+class SeriesApi:
+
+    def __init__(self, client: ApiClient):
+
+        self.client = client
+
+    def all(self):
+
+        return self.client.get("/v3/Series")
+
+    def by_id(self, series_id: int):
+
+        return self.client.get(
+            f"/v3/Series/{series_id}"
+        )
