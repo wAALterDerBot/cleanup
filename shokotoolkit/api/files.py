e026621be376d2cf10fae90f0fa751fc5cb044bd
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 02:19:54 +0200

    Update files.py

--- shokotoolkit/api/files.py
+++ shokotoolkit/api/files.py
@@ -1,1 +1,14 @@
+from __future__ import annotations
 
+from anime_cleanup.api.client import ApiClient
+
+
+class FilesApi:
+
+    def __init__(self, client: ApiClient):
+
+        self.client = client
+
+    def all(self):
+
+        return self.client.get("/v3/File")
