377bcbee647a4110ac998c9e537192e977112834
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 10:56:42 +0200

    Create main.py

---
+++ shokotoolkit/main.py
@@ -0,0 +1,16 @@
+from rich import print
+
+from shokotoolkit import __version__
+
+
+def main():
+
+    print()
+
+    print(f"[bold cyan]Shoko Toolkit[/bold cyan] v{__version__}")
+
+    print()
+
+    print("Library Management Toolkit")
+
+    print()
