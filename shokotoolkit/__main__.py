5a9053b603ddfca01de00181ab623760a12d382a
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 11:02:40 +0200

    Create __main__.py

---
+++ shokotoolkit/__main__.py
@@ -0,0 +1,15 @@
+"""
+Shoko Toolkit
+
+Package entry point.
+
+Allows running the toolkit with:
+
+    python -m shokotoolkit
+"""
+
+from shokotoolkit.main import main
+
+
+if __name__ == "__main__":
+    main()
