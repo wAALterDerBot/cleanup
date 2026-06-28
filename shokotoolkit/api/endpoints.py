da9a39ed3bd8703abe492aca024bce4c820b03f5
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 18:58:09 +0200

    Update endpoints.py

--- shokotoolkit/api/endpoints.py
+++ shokotoolkit/api/endpoints.py
@@ -1,13 +1,17 @@
 """
-API endpoint definitions.
+Shoko API endpoint definitions.
 """
 
 API_PREFIX = "/api/v3"
 
 AUTH = f"{API_PREFIX}/Auth"
 
+SYSTEM = f"{API_PREFIX}/System"
+
 SERIES = f"{API_PREFIX}/Series"
 
 FILES = f"{API_PREFIX}/File"
 
-RELEASE_MANAGEMENT = f"{API_PREFIX}/Action/Release"
+EPISODES = f"{API_PREFIX}/Episode"
+
+RELEASE = f"{API_PREFIX}/Action/Release"
