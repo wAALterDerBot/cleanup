ea0967f72e006055ae01ab1a47bc454cdd2b57b3
wAALterDerBot <just.an.oniichan@gmail.com>
2026-06-28 11:35:30 +0200

    Create endpoints.py

---
+++ shokotoolkit/api/endpoints.py
@@ -0,0 +1,13 @@
+"""
+API endpoint definitions.
+"""
+
+API_PREFIX = "/api/v3"
+
+AUTH = f"{API_PREFIX}/Auth"
+
+SERIES = f"{API_PREFIX}/Series"
+
+FILES = f"{API_PREFIX}/File"
+
+RELEASE_MANAGEMENT = f"{API_PREFIX}/Action/Release"
